4760730ce78360608ada7bb4be690c335b52f8bc
dunas26 <jhonjamesillo@gmail.com>
2021-10-07 11:28:08 -0500

    UPDATE: Home module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: '**', loadChildren: () => import('./components/public/page-not-found/page-not-found.module').then(m => m.PageNotFoundModule) }];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./components/public/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: '**',
+    loadChildren: () =>
+      import('./components/public/page-not-found/page-not-found.module').then(
+        (m) => m.PageNotFoundModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
