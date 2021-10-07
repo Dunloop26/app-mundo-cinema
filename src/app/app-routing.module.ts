256b8772e4f763f4e15157695bb74e3d6fe09d54
dunas26 <jhonjamesillo@gmail.com>
2021-10-07 10:51:49 -0500

    UPDATE: Page styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '**', loadChildren: () => import('./components/public/page-not-found/page-not-found.module').then(m => m.PageNotFoundModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
