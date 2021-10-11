67a01aabda2dd5fe0ab823cdfbefeba24608501f
dunas26 <jhonjamesillo@gmail.com>
2021-10-11 06:53:40 -0500

    UPDATE: Invoice Details Component

---
+++ src/app/components/shared/invoice-details/invoice-details.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { InvoiceProduct } from 'src/app/interfaces/invoice-product';
+
+@Component({
+  selector: 'app-invoice-details',
+  templateUrl: './invoice-details.component.html',
+  styleUrls: ['./invoice-details.component.scss'],
+})
+export class InvoiceDetailsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  @Input() id: string = '';
+  @Input() products: Array<InvoiceProduct> = [];
+  @Input() invoiceTotal: number = 0;
+
+  zeroFill(value: string){
+    const pad = '00000000';
+    const result = (pad + value).slice(-pad.length);
+    return result;
+  }
+}
